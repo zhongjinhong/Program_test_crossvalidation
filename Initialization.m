7f1c9318a34df4df5c9a8492042880540692cd17
zjh4213 <jinhong@mail.ustc.edu.cn>
2016-11-14 15:35:41 +0800

    modified from my macbook pro

--- Initialization.m
+++ Initialization.m
@@ -1,183 +1,153 @@
+total_repeat_num=20;
+begin_num=1;
+end_num=1;
+% count=zeros(2*end_num*step_num,(end_num-begin_num+1)*total_repeat_num); 
 switch experiment_num
     case 1
         input_file_dir='../Input Data/Adult/Clustering/';
         output_file_dir='../Output Data/Adult/Clustering/';
-        total_repeat_num=10;begin_num=1;end_num=10;step_num = 100;count=zeros(2*end_num*step_num,(end_num-begin_num+1)*total_repeat_num); 
         title_name='Adult(Yan Yan''s Data)'; 
     case 2
         input_file_dir='../Input Data/Adult/latent model/';
         output_file_dir='../Output Data/Adult/latent model/';
-        total_repeat_num=10;begin_num=1;end_num=10;step_num = 100;count=zeros(2*end_num*step_num,(end_num-begin_num+1)*total_repeat_num);  
         title_name='Adult(Raykar''s Data)'; 
     case 3
         input_file_dir='../Input Data/Adult/expertise_difficult_model/';
         output_file_dir='../Output Data/Adult/expertise_difficult_model/';
-        total_repeat_num=10;begin_num=1;end_num=10;step_num = 100;count=zeros(2*end_num*step_num,(end_num-begin_num+1)*total_repeat_num);  
         title_name='Adult(Whitehill''s Data)'; 
-        
     case 4
         input_file_dir='../Input Data/Conect/Clustering/';    
         output_file_dir='../Output Data/Conect/Clustering/';
-        total_repeat_num=10;begin_num=1;end_num=10;step_num = 100;count=zeros(2*end_num*step_num,(end_num-begin_num+1)*total_repeat_num); 
         title_name=('Conect(Yan Yan''s Data)'); 
     case 5
         input_file_dir='../Input Data/Conect/latent model/';
         output_file_dir='../Output Data/Conect/latent model/';
-        total_repeat_num=10;begin_num=1;end_num=10;step_num = 100;count=zeros(2*end_num*step_num,(end_num-begin_num+1)*total_repeat_num); 
         title_name='Conect(Raykar''s Data)';
     case 6
         input_file_dir='../Input Data/Conect/expertise_difficult_model/';
         output_file_dir='../Output Data/Conect/expertise_difficult_model/';
-        total_repeat_num=10;begin_num=1;end_num=10;step_num = 100;count=zeros(2*end_num*step_num,(end_num-begin_num+1)*total_repeat_num); 
         title_name='Conect(Whitehill''s Data)'; 
                 
-        
-        
     case 7
         input_file_dir='../Input Data/mushroom/Clustering/';
         output_file_dir='../Output Data/mushroom/Clustering/';
-        total_repeat_num=10;begin_num=1;end_num=10;step_num = 100;count=zeros(2*end_num*step_num,(end_num-begin_num+1)*total_repeat_num); 
         title_name='mushroom(Yan Yan''s Data)';
     case 8
         input_file_dir='../Input Data/mushroom/latent model/';
         output_file_dir='../Output Data/mushroom/latent model/';
-        total_repeat_num=10;begin_num=1;end_num=10;step_num = 100;count=zeros(2*end_num*step_num,(end_num-begin_num+1)*total_repeat_num);
         title_name='mushroom(Raykar''s Data)';        
     case 9
         input_file_dir='../Input Data/mushroom/expertise_difficult_model/';
         output_file_dir='../Output Data/mushroom/expertise_difficult_model/';
-        total_repeat_num=10;begin_num=1;end_num=10;step_num = 100;count=zeros(2*end_num*step_num,(end_num-begin_num+1)*total_repeat_num);
         title_name='mushroom(Whitehill''s Data)';
 
-        
-        
     case 10
         input_file_dir='../Input Data/test_data/Clustering/';
         output_file_dir='../Output Data/test_data/Clustering/';
-        total_repeat_num=10;begin_num=1;end_num=10;step_num = 100;count=zeros(2*end_num*step_num,(end_num-begin_num+1)*total_repeat_num); 
         title_name='test_data(Yan Yan''s Data)'; 
     case 11
         input_file_dir='../Input Data/test_data/latent model/';
         output_file_dir='../Output Data/test_data/latent model/';
-        total_repeat_num=10;begin_num=1;end_num=10;step_num = 100;count=zeros(2*end_num*step_num,(end_num-begin_num+1)*total_repeat_num);  
         title_name='test_data(Raykar''s Data)'; 
     case 12
         input_file_dir='../Input Data/test_data/expertise_difficult_model/';
         output_file_dir='../Output Data/test_data/expertise_difficult_model/';
-        total_repeat_num=10;begin_num=1;end_num=10;step_num = 100;count=zeros(2*end_num*step_num,(end_num-begin_num+1)*total_repeat_num);  
         title_name='test_data(Whitehill''s Data)';         
-        
-        
         
     case 21 %%total_repeat_num=10 needs about 12h
         input_file_dir='../Input Data/Real Data/Clustering/';
         output_file_dir='../Output Data/Real Data/Clustering/';
-        total_repeat_num=10;begin_num=1;end_num=10;step_num = 100;count=zeros(2*end_num*step_num,total_repeat_num);
         title_name='Twitter Topic(Yan Yan''s Data)';
     case 22
         input_file_dir='../Input Data/Real Data/latent model/';
         output_file_dir='../Output Data/Real Data/latent model/';
-        total_repeat_num=10;begin_num=1;end_num=10;step_num = 100;count=zeros(2*end_num*step_num,total_repeat_num);
         title_name='Twitter Topic(Raykar''s Data)';
     case 23
         input_file_dir='../Input Data/Real Data/expertise_difficult_model/';
         output_file_dir='../Output Data/Real Data/expertise_difficult_model/';
-        total_repeat_num=10;begin_num=1;end_num=10;step_num = 100;count=zeros(2*end_num*step_num,total_repeat_num);
         title_name='Twitter Topic(Whitehill''s Data)';
     case 24
         input_file_dir='../Input Data/Real Data/Real Label/';
         output_file_dir='../Output Data/Real Data/Real Label/';
-        total_repeat_num=10;begin_num=1;end_num=10;step_num = 100;count=zeros(2*end_num*step_num,total_repeat_num);
         title_name='Twitter Topic(Real Data)';
     
-        
-        
     case 25
         input_file_dir='../Input Data/Real_Data_AAAI_balance/Clustering/';
         output_file_dir='../Output Data/Real_Data_AAAI_balance/Clustering/';
-        total_repeat_num=10;begin_num=1;end_num=10;step_num = 100;count=zeros(2*end_num*step_num,total_repeat_num);
         title_name='NER(Clustering)';       
     case 26
         input_file_dir='../Input Data/Real_Data_AAAI_balance/latent model/';
         output_file_dir='../Output Data/Real_Data_AAAI_balance/latent model/';
-        total_repeat_num=10;begin_num=1;end_num=10;step_num = 100;count=zeros(2*end_num*step_num,total_repeat_num);
         title_name='NER(Raykar''s Data)';                  
     case 27
         input_file_dir='../Input Data/Real_Data_AAAI_balance/expertise_difficult_model/';
         output_file_dir='../Output Data/Real_Data_AAAI_balance/expertise_difficult_model/';
-        total_repeat_num=10;begin_num=1;end_num=10;step_num = 100;count=zeros(2*end_num*step_num,total_repeat_num);
         title_name='NER(Whitehill''s Data)';       
     case 28
         input_file_dir='../Input Data/Real_Data_AAAI_balance/Real Label/';
         output_file_dir='../Output Data/Real_Data_AAAI_balance/Real Label/';
-        total_repeat_num=10;begin_num=1;end_num=10;step_num = 100;count=zeros(2*end_num*step_num,total_repeat_num);
         title_name='NER(Real Data)';           
         
-
-        
     case 41
-        input_file_dir ='../Input Data/Real Data Standar/Clustering/';
+        input_file_dir ='../Input Data/Real Data/Clustering/';
         output_file_dir='../Output Data/Real Data Replicate/Clustering/';
-        total_repeat_num=30;begin_num=1;end_num=11;step_num = 100;count=zeros(2*1000,(end_num-begin_num+1)*total_repeat_num);
+        total_repeat_num=30;end_num=11;
         title_name='Computing time(Yan Yan''s Data)';        
     case 42
-        input_file_dir ='../Input Data/Real Data Standar/latent model/';
+        input_file_dir ='../Input Data/Real Data/latent model/';
         output_file_dir='../Output Data/Real Data Replicate/latent model/';
-        total_repeat_num=30;begin_num=1;end_num=11;step_num = 100;count=zeros(2*1000,(end_num-begin_num+1)*total_repeat_num);
+        total_repeat_num=30;end_num=11;
         title_name='Computing time(Raykar''s Data)'; 
     case 43
-        input_file_dir ='../Input Data/Real Data Standar/expertise_difficult_model/';
+        input_file_dir ='../Input Data/Real Data/expertise_difficult_model/';
         output_file_dir='../Output Data/Real Data Replicate/expertise_difficult_model/';
-        total_repeat_num=30;begin_num=1;end_num=11;step_num = 100;count=zeros(2*1000,(end_num-begin_num+1)*total_repeat_num);
+        total_repeat_num=30;end_num=11;
         title_name='Computing time(Whitehill''s Data)'; 
     case 44
-        input_file_dir ='../Input Data/Real Data Standar/Real Label/';
+        input_file_dir ='../Input Data/Real Data/Real Label/';
         output_file_dir='../Output Data/Real Data Replicate/Real Label/';
-        total_repeat_num=30;begin_num=1;end_num=11;step_num = 100;count=zeros(2*1000,(end_num-begin_num+1)*total_repeat_num);
+        total_repeat_num=30;end_num=11;
         title_name='Computing time(Real Data)';    
-        
-    
+
     case 45
-        input_file_dir ='../Input Data/Real Data Standar/Clustering/';
+        input_file_dir ='../Input Data/Real Data/Clustering/';
         output_file_dir='../Output Data/Real Data Random1/Clustering/';
-        total_repeat_num=100;begin_num=1;end_num=11;step_num = 100;count=zeros(2*1000,(end_num-begin_num+1)*total_repeat_num);
+        total_repeat_num=100;end_num=11;
         title_name='Twitter Topic(Clustering data)';        
     case 46
-        input_file_dir ='../Input Data/Real Data Standar/latent model/';
+        input_file_dir ='../Input Data/Real Data/latent model/';
         output_file_dir='../Output Data/Real Data Random1/latent model/';
-        total_repeat_num=100;begin_num=1;end_num=11;step_num = 100;count=zeros(2*1000,(end_num-begin_num+1)*total_repeat_num);
+        total_repeat_num=100;end_num=11;
         title_name='Twitter Topic(Raykar''s Data)'; 
     case 47
-        input_file_dir ='../Input Data/Real Data Standar/expertise_difficult_model/';
+        input_file_dir ='../Input Data/Real Data/expertise_difficult_model/';
         output_file_dir='../Output Data/Real Data Random1/expertise_difficult_model/';
-        total_repeat_num=100;begin_num=1;end_num=11;step_num = 100;count=zeros(2*1000,(end_num-begin_num+1)*total_repeat_num);
+        total_repeat_num=100;end_num=11;
         title_name='Twitter Topic(Whitehill''s Data)'; 
     case 48
-        input_file_dir ='../Input Data/Real Data Standar/Real Label/';
+        input_file_dir ='../Input Data/Real Data/Real Label/';
         output_file_dir='../Output Data/Real Data Random1/Real Label/';
-        total_repeat_num=100;begin_num=1;end_num=11;step_num = 100;count=zeros(2*1000,(end_num-begin_num+1)*total_repeat_num);
+        total_repeat_num=100;end_num=11;
         title_name='Twitter Topic(Real Label)';  
 
     case 49
-        input_file_dir ='../Input Data/Real Data Standar/Clustering/';
+        input_file_dir ='../Input Data/Real Data/Clustering/';
         output_file_dir='../Output Data/Real Data Random2/Clustering/';
-        total_repeat_num=30;begin_num=1;end_num=11;step_num = 100;count=zeros(2*1000,(end_num-begin_num+1)*total_repeat_num);
+        total_repeat_num=30;end_num=11;
         title_name='Twitter Topic(Yan Yan''s Data)';        
     case 50
-        input_file_dir ='../Input Data/Real Data Standar/latent model/';
+        input_file_dir ='../Input Data/Real Data/latent model/';
         output_file_dir='../Output Data/Real Data Random2/latent model/';
-        total_repeat_num=30;begin_num=1;end_num=11;step_num = 100;count=zeros(2*1000,(end_num-begin_num+1)*total_repeat_num);
+        total_repeat_num=30;end_num=11;
         title_name='Twitter Topic(Raykar''s Data)'; 
     case 51
-        input_file_dir ='../Input Data/Real Data Standar/expertise_difficult_model/';
+        input_file_dir ='../Input Data/Real Data/expertise_difficult_model/';
         output_file_dir='../Output Data/Real Data Random2/expertise_difficult_model/';
-        total_repeat_num=30;begin_num=1;end_num=11;step_num = 100;count=zeros(2*1000,(end_num-begin_num+1)*total_repeat_num);
+        total_repeat_num=30;end_num=11;
         title_name='Twitter Topic(Whitehill''s Data)'; 
     case 52
-        input_file_dir ='../Input Data/Real Data Standar/Real Label/';
+        input_file_dir ='../Input Data/Real Data/Real Label/';
         output_file_dir='../Output Data/Real Data Random2/Real Label/';
-        total_repeat_num=30;begin_num=11;end_num=11;step_num = 100;count=zeros(2*1000,(end_num-begin_num+1)*total_repeat_num);
+        total_repeat_num=30;end_num=11;
         title_name='Twitter Topic(Real Data)';          
-
 end
-total_repeat_num = 3;end_num=3;
--- compare.m
+++ compare.m
@@ -1,52 +1,32 @@
 function [  ] = compare( experiment_num )
+
     Initialization();
     svm_para=sprintf('%s','-s 0 -t 0');
  
     for num=begin_num:end_num
-        for repeat_num=1:total_repeat_num
+        for repeat_num=1:total_repeat_num           
+            file_name=sprintf('%s%s%d%s',input_file_dir,'X_',repeat_num,'.mat');
+            load(file_name); 
+            file_name=sprintf('%s%s%d%s',input_file_dir,'Y_',repeat_num,'.mat');
+            load(file_name); 
+            %%%%%%%% load the ground-truth labels %%%%%%%%%%%%%%%%
+            file_name=sprintf('%s%s%d%s',input_file_dir,'Z_',repeat_num,'.mat');
+            load(file_name); 
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%            
+            index = find(sum(Y~=-2,2)>0);
+            X = X(index,:);
+            Y = Y(index,:);  
+            Z = Z(index,:);
+            [n,expert_num] = size(Y);
             switch experiment_num
-                case {1,2,3,4,5,6,7,8,9,10,11,12,21,22,23,24,25,26,27,28,29,30,31,32}
-                    file_name=sprintf('%s%s%d%s%d%s',input_file_dir,'X_',num*step_num,'_',repeat_num,'.mat');
-                    load(file_name);  
-                    file_name=sprintf('%s%s%d%s%d%s',input_file_dir,'Y_',num*step_num,'_',repeat_num,'.mat');
-                    load(file_name);  
-       
-%                 case {11}
-%                     file_name=sprintf('%s%s',input_file_dir,'X_1000_1.mat');
-%                     load(file_name);  
-%                     file_name=sprintf('%s%s%s',input_file_dir,'Y_1000_1','.mat');
-%                     load(file_name);
-%                     Y_temp=Y;
-%                     for i = 1:num-1
-%                         Y = [Y Y_temp];
-%                     end
-                    
                 case {41,42,43,44}
-                    file_name=sprintf('%s%s%d%s',input_file_dir,'X_',repeat_num,'.mat');
-                    load(file_name); 
-                    file_name=sprintf('%s%s%d%s',input_file_dir,'Y_',repeat_num,'.mat');
-                    load(file_name); 
-                    
                     Y_temp=Y;
                     for i = 1:num-1
                         Y = [Y Y_temp];
-                    end                    
-                    
-                    
+                    end              
                 case {45,46,47,48}
-                    file_name=sprintf('%s%s%d%s',input_file_dir,'X_',repeat_num,'.mat');
-                    load(file_name); 
-                    file_name=sprintf('%s%s%d%s',input_file_dir,'Y_',repeat_num,'.mat');
-                    load(file_name); 
-                    
-                    index = find(sum(Y~=-2,2)>0);
-                    X = X(index,:);
-                    Y = Y(index,:);                      
-                    
-                    [n,expert_num] = size(Y);
                     rn_temp = rand(n,expert_num*(num-1));
                     Y_temp = zeros(n,expert_num*(num-1));
-                    
                     probability_temp = 0.5;
                     for i=1:n
                         for t=1:expert_num*(num-1)
@@ -59,25 +39,8 @@ function [  ] = compare( experiment_num )
                     end  
 
                     Y = [Y Y_temp];     
-                    
-                    
+
                 case {49,50,51,52}
-                    file_name=sprintf('%s%s%d%s',input_file_dir,'X_',repeat_num,'.mat');
-                    load(file_name); 
-                    file_name=sprintf('%s%s%d%s',input_file_dir,'Y_',repeat_num,'.mat');
-                    load(file_name); 
-                    %%%%%%%%需要加load 每个X对应的ground_truth.%%%%%%%%%%%%%%%%
-                    file_name=sprintf('%s%s%d%s',input_file_dir,'Z_',repeat_num,'.mat');
-                    load(file_name); 
-                    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-                    
-                    index = find(sum(Y~=-2,2)>0);
-                    X = X(index,:);
-                    Y = Y(index,:);  
-                    Z = Z(index,:);
-
-
-                    [n,expert_num] = size(Y);
                     Y_temp = zeros(n,expert_num*(num-1));
                     
                     index = randperm(n);
@@ -92,23 +55,11 @@ function [  ] = compare( experiment_num )
                         Y_temp(1:wrong_num,t) = - Y_temp(1:wrong_num,t);
                     end  
 
-                    Y = [Y Y_temp];                       
-
+                    Y = [Y Y_temp];     
             end
 
-            
-            index = find(sum(Y~=-2,2)>0);
-            X = X(index,:);
-            Y = Y(index,:);              
-            
-            
-            
-            n=size(X,1);
-            expert_num=size(Y,2);
 
-
-            
-            if experiment_num == 32
+            if experiment_num == 28
                 filtered_annotator =[3 7 15 18 47 58 63 66 68 69 75 80 93 111 151 153 158 159 184 193 194 201 204 210 229];
                 filter_num = size(filtered_annotator,2);
                 for j = 1:filter_num
@@ -134,10 +85,7 @@ function [  ] = compare( experiment_num )
             W_M3V( (num-begin_num)*total_repeat_num+repeat_num,: )=M3V(X,Y,svm_para);
             Time_M3V ((num-begin_num)*total_repeat_num+repeat_num)= toc;          
             tic
-%             [W_LCM( (num-begin_num)*total_repeat_num+repeat_num,: ),count( 1:2*n,(num-begin_num)*total_repeat_num+repeat_num )]=LCM_test_linear(X,Y,svm_para);
-%             Time_Soft_LCM((num-begin_num)*total_repeat_num+repeat_num)= toc;
 
-            
             W_LCM1( (num-begin_num)*total_repeat_num+repeat_num,: ) =LCM(X,Y,svm_para);    
             W_LCM2( (num-begin_num)*total_repeat_num+repeat_num,: ) =LCM_compare(X,Y,svm_para);  
 
