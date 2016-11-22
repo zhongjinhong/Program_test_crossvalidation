804a647d3763c5da1dd5a0f635d247a20508ca74
zjh4213 <jinhong@mail.ustc.edu.cn>
2016-11-22 19:44:05 +0800

    modified from my macbook pro

--- Produce_data/produce_all_data.m
+++ Produce_data/produce_all_data.m
@@ -2,33 +2,33 @@ function [  ] = produce_all_data( data_num, experiment_num)
 	labeled_num = 5;
     switch data_num
         case 1
-            load 'Adult/Adult.mat';   
-            data_string = 'Adult/';
+            load '../../Input Data/Adult/Adult.mat';   
+            data_string = '../../Input Data/Adult/';
         case 2
-            load 'Conect/conect-4.mat';
-            data_string = 'Conect/';
+            load '../../Input Data/Conect/conect-4.mat';
+            data_string = '../../Input Data/Conect/';
         case 3
-            load 'mushroom/mushroom.mat';
-            data_string = 'mushroom/';
+            load '../../Input Data/mushroom/mushroom.mat';
+            data_string = '../../Input Data/mushroom/';
         case 4
-            load 'Real Data/realdata2.mat';
-            data_string = 'Real Data/';
+            load '../../Input Data/Real Data/realdata2.mat';
+            data_string = '../../Input Data/Real Data/';
         case 5
-            load 'test_data/dna.mat';
-            data_string = 'test_data/';
+            load '../../Input Data/test_data/dna.mat';
+            data_string = '../../Input Data/test_data/';
 %         case 6
-%             load 'tiantian/tiantian.mat';
-%             data_string = 'tiantian/';
+%             load '../../Input Data/tiantian/tiantian.mat';
+%             data_string = '../../Input Data/tiantian/';
 %         case 7
-%             load 'Real_Data_AAAI/real_data_aaai.mat';
-%             data_string = 'Real_Data_AAAI/';
+%             load '../../Input Data/Real_Data_AAAI/real_data_aaai.mat';
+%             data_string = '../../Input Data/Real_Data_AAAI/';
         case 8
-            load 'Real_Data_AAAI_balance/real_data_aaai_balance.mat';
-            data_string = 'Real_Data_AAAI_balance/';
+            load 'load '../../Input Data/Adult/Adult.mat'; Real_Data_AAAI_balance/real_data_aaai_balance.mat';
+            data_string = '../../Input Data/Real_Data_AAAI_balance/';
             
         case 11
-            load 'Real Data/realdata1.mat';
-            data_string = 'Real Data/';
+            load 'load '../../Input Data/Adult/Adult.mat'; Real Data/realdata1.mat';
+            data_string = '../../Input Data/Real Data/';
     end
     
     
